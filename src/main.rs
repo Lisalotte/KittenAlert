b07bdf304aac039c0f4ba3b35c7f147058e6fa4a
Lisalotte <lisa.pothoven@xs4all.nl>
2021-02-18 14:19:24 +0100

    Test for timer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+extern crate timer;
+extern crate chrono;
+
 use teloxide::prelude::*;
 use std::env;
+
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
@@ -13,12 +19,29 @@ async fn run() {
     log::info!("Starting dices_bot...");
 
     let bot = Bot::from_env();
+    
+    let timer = timer::Timer::new();
+    // Number of times the callback has been called.
+    let count = Arc::new(Mutex::new(0));
+
+    // Start repeating. Each callback increases `count`.
+    let guard = {
+        let count = count.clone();
+        timer.schedule_repeating(chrono::Duration::seconds(5), move || {
+            *count.lock().unwrap() += 1;
+            let count_result = *count.lock().unwrap();
+            log::info!("The timer was called {} times", count_result);
+        })
+    };
 
     teloxide::repl(bot, |message| async move {
         message.answer_dice().send().await?;
         ResponseResult::<()>::Ok(())
     })
     .await;
+
+    // Now drop the guard. This should stop the timer.
+    drop(guard);
 
     // Receive incoming updates using long polling
     // bot.get_updates();
