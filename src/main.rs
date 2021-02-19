080eacf59baf3cc0da2feff4e647fdb3e28c0e16
Lisalotte <lisa.pothoven@xs4all.nl>
2021-02-19 11:01:01 +0100

    Fixed tokio version errors, timer and webrequest now working

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,38 @@ extern crate chrono;
 
 use teloxide::prelude::*;
 use std::env;
+use tokio::time;
+use std::time::{Duration, Instant, SystemTime};
 
 use std::thread;
 use std::sync::{Arc, Mutex};
 
-#[tokio::main]
-async fn main() {
-    run().await;
+async fn webrequest() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://www.rust-lang.org").await?;
+
+    log::info!("Status: {}", res.status());
+    let body = res.text().await?;
+    log::info!("Body:\n\n{}", body);
+
+    Ok(())
 }
 
-async fn run() {
+#[tokio::main]
+async fn main() {
     env::set_var("TELOXIDE_TOKEN", "1671683413:AAEbPdktygghJI0HNE3aGy2FC67nmUoq75U");
 
     teloxide::enable_logging!();
     log::info!("Starting dices_bot...");
 
     let bot = Bot::from_env();
-    
-    let timer = timer::Timer::new();
-    // Number of times the callback has been called.
-    let count = Arc::new(Mutex::new(0));
 
-    // Start repeating. Each callback increases `count`.
-    let guard = {
-        let count = count.clone();
-        timer.schedule_repeating(chrono::Duration::seconds(5), move || {
-            *count.lock().unwrap() += 1;
-            let count_result = *count.lock().unwrap();
-            log::info!("The timer was called {} times", count_result);
-        })
-    };
+    tokio::spawn( async move {
+        let mut interval = time::interval(Duration::from_millis(2000));
+        for _i in 0..5 {
+            interval.tick().await;
+            webrequest().await;
+        }
+    });
 
     teloxide::repl(bot, |message| async move {
         message.answer_dice().send().await?;
@@ -40,9 +42,4 @@ async fn run() {
     })
     .await;
 
-    // Now drop the guard. This should stop the timer.
-    drop(guard);
-
-    // Receive incoming updates using long polling
-    // bot.get_updates();
 }
