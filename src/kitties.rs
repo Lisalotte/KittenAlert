a7dc8b21fbbf4d80ce70f99c4d36ed1789ede78b
Lisalotte <lisa.pothoven@xs4all.nl>
2021-02-25 22:45:52 +0100

    sending messages with titles of kitties now works

--- src/kitties.rs
+++ src/kitties.rs
@@ -1,10 +1,31 @@
 use serde::{Serialize, Deserialize};
+use scraper::{Html, Selector};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Kitty {}
+pub struct Kitty {
+    pub title: String,
+}
 
 pub fn from_page(body: String) -> Vec<Kitty> {
-    let list = Vec::new();
+    let mut list = Vec::new();
+   
+    let body: &str = body.as_str();
+    let document = Html::parse_document(body);
+    let listings = Selector::parse("li").unwrap();
+
+    for element in document.select(&listings) {
+        assert_eq!("li", element.value().name());
+        let h3_selector = Selector::parse("h3").unwrap();
+        for title in element.select(&h3_selector) {
+            assert_eq!("h3", title.value().name());
+            //log::info!("{}", title.inner_html());
+            let kitty = Kitty { 
+                title: title.inner_html(),
+            };
+            list.push(kitty);
+        }      
+    }
+
     list
 }
 
@@ -24,8 +45,18 @@ impl Kitty {
 
 #[test]
 fn test_from_page() {
-    let page = include_str!("../test_page.txt");
-    let kitties = from_page(page.to_string());
-    dbg!(kitties);
-    assert!(false);
+    let filename = "test_page.txt";
+    let content = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    
+    let body: &str = content.as_str();
+
+    let document = Html::parse_document(body);
+    let listings = Selector::parse("li").unwrap();
+
+    for element in document.select(&listings) {
+        assert_eq!("li", element.value().name());
+        log::info!("{}", element.value().name());
+    }
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use timer;
-use chrono;
-
 use teloxide::prelude::*;
 use std::env;
 use tokio::time;
 use std::time::Duration;
 use sled::Db;
+use scraper::{Html, Selector};
+use std::fs;
 
 mod kitties;
 
@@ -15,12 +14,13 @@ async fn webrequest() -> Result<String, reqwest::Error> {
 
     log::info!("Status: {}", res.status());
     let body = res.text().await?;
-    log::info!("Body:\n\n{}", body);
 
     Ok(body)
 }
-async fn show(kitty: kitties::Kitty) {
-    todo!()
+async fn show(kitty: kitties::Kitty, bot: teloxide::Bot) {
+    let id = 249861073;
+    //log::info!("{}", kitty.title);
+    bot.send_message(id, kitty.title).send().await.unwrap();
 }
 
 async fn kittenloop(bot: teloxide::Bot) {
@@ -38,13 +38,37 @@ async fn kittenloop(bot: teloxide::Bot) {
         for kitty in list {
             if !kitty.seen(&db) {
                 db.insert(kitty.as_bytes(), "").unwrap();
+                //log::info!("db {}", db.len());
+                show(kitty, bot.clone()).await;
             } else {
-                show(kitty).await;
+                log::info!("Kitty already in database!");
+                //show(kitty).await;
             }
         }
         interval.tick().await;
     }
 }
+/*
+fn test_document_parsing() {
+    let filename = "test_page.txt";
+    let content = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    
+    let body: &str = content.as_str();
+
+    let document = Html::parse_document(body);
+    let listings = Selector::parse("li").unwrap();
+
+    for element in document.select(&listings) {
+        assert_eq!("li", element.value().name());
+        let h3_selector = Selector::parse("h3").unwrap();
+        for title in element.select(&h3_selector) {
+            assert_eq!("h3", title.value().name());
+            log::info!("{}", title.inner_html());
+        }      
+    }
+}
+*/
 
 #[tokio::main]
 async fn main() {
