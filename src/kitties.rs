12d9fc61d63a4b57126a5a864bf97d4cb325ba8a
Lisalotte <lisa.pothoven@xs4all.nl>
2021-02-26 10:29:47 +0100

    Kitties working!

--- src/kitties.rs
+++ src/kitties.rs
@@ -4,6 +4,50 @@ use scraper::{Html, Selector};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Kitty {
     pub title: String,
+    pub date: String,
+    pub url: String,
+}
+
+fn is_ad(html: scraper::ElementRef) -> bool {
+    let selector = Selector::parse("span").unwrap();
+    for footer in html.select(&selector) {   
+        for class in footer.value().classes() {
+            if class.eq("mp-Listing-seller-link") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+fn find_date(html: scraper::ElementRef) -> String {
+    let selector = Selector::parse("span").unwrap();
+    for footer in html.select(&selector) {   
+        for class in footer.value().classes() {
+            if class.eq("mp-Listing-date") {
+                let text = footer.inner_html();
+                return text;
+            }
+        }
+    }
+    return String::from("");
+}
+
+fn find_url(html: scraper::ElementRef) -> String {
+    let selector = Selector::parse("a").unwrap();
+    let mut url = String::new();
+    for link in html.select(&selector) {
+        for class in link.value().classes() {
+            if class.eq("mp-Listing-coverLink") {
+                url = format!(
+                    "{}{}", 
+                    "https://www.marktplaats.nl/",
+                    link.value().attr("href").unwrap()
+                ).to_string();
+            }
+        }
+    }
+    return url;
 }
 
 pub fn from_page(body: String) -> Vec<Kitty> {
@@ -15,14 +59,21 @@ pub fn from_page(body: String) -> Vec<Kitty> {
 
     for element in document.select(&listings) {
         assert_eq!("li", element.value().name());
+
         let h3_selector = Selector::parse("h3").unwrap();
-        for title in element.select(&h3_selector) {
-            assert_eq!("h3", title.value().name());
-            //log::info!("{}", title.inner_html());
-            let kitty = Kitty { 
-                title: title.inner_html(),
-            };
-            list.push(kitty);
+        if !is_ad(element) {
+            for title in element.select(&h3_selector) {
+                assert_eq!("h3", title.value().name());
+
+                let url = find_url(element);
+                let date = find_date(element);
+                let kitty = Kitty { 
+                    title: title.inner_html(),
+                    date: date,
+                    url: url,
+                };
+                list.push(kitty);
+            }
         }      
     }
 
@@ -33,12 +84,13 @@ impl Kitty {
     pub fn as_bytes(&self) -> Vec<u8> {
         bincode::serialize(self).unwrap()
     }
+    /*
     fn from_bytes(bytes: &[u8]) -> Self {
         bincode::deserialize(bytes).unwrap()
     }
-
+    */
     pub fn seen(&self, db: &sled::Tree) -> bool {
-        let bytes = self.as_bytes();
+        let bytes = self.title.as_bytes();
         db.contains_key(bytes).unwrap()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use teloxide::prelude::*;
+use teloxide::{types, BotBuilder};
 use std::env;
 use tokio::time;
 use std::time::Duration;
 use sled::Db;
-use scraper::{Html, Selector};
-use std::fs;
 
 mod kitties;
 
@@ -12,15 +11,20 @@ async fn webrequest() -> Result<String, reqwest::Error> {
     let url = "https://www.marktplaats.nl/l/dieren-en-toebehoren/katten-en-kittens-overige-katten/#searchInTitleAndDescription:true";
     let res = reqwest::get(url).await?;
 
-    log::info!("Status: {}", res.status());
+    //log::info!("Status: {}", res.status());
     let body = res.text().await?;
 
     Ok(body)
 }
 async fn show(kitty: kitties::Kitty, bot: teloxide::Bot) {
     let id = 249861073;
-    //log::info!("{}", kitty.title);
-    bot.send_message(id, kitty.title).send().await.unwrap();
+    let message = format!(
+        "<b>{}</b>\n{}\n<a href='{}'>Linkje</a>", 
+        kitty.title.as_str(), 
+        kitty.date.as_str(), 
+        kitty.url.as_str()
+    );
+    bot.send_message(id, message).send().await.unwrap();
 }
 
 async fn kittenloop(bot: teloxide::Bot) {
@@ -31,44 +35,19 @@ async fn kittenloop(bot: teloxide::Bot) {
         .unwrap();
     bot.send_message(id, "Hello World").send().await.unwrap();
     
-    let mut interval = time::interval(Duration::from_secs(60));
+    let mut interval = time::interval(Duration::from_secs(120));
     loop {
         let page = webrequest().await.unwrap();
         let list = kitties::from_page(page);
         for kitty in list {
             if !kitty.seen(&db) {
-                db.insert(kitty.as_bytes(), "").unwrap();
-                //log::info!("db {}", db.len());
+                db.insert(kitty.title.as_bytes(), kitty.as_bytes()).unwrap();
                 show(kitty, bot.clone()).await;
-            } else {
-                log::info!("Kitty already in database!");
-                //show(kitty).await;
             }
         }
         interval.tick().await;
     }
 }
-/*
-fn test_document_parsing() {
-    let filename = "test_page.txt";
-    let content = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-    
-    let body: &str = content.as_str();
-
-    let document = Html::parse_document(body);
-    let listings = Selector::parse("li").unwrap();
-
-    for element in document.select(&listings) {
-        assert_eq!("li", element.value().name());
-        let h3_selector = Selector::parse("h3").unwrap();
-        for title in element.select(&h3_selector) {
-            assert_eq!("h3", title.value().name());
-            log::info!("{}", title.inner_html());
-        }      
-    }
-}
-*/
 
 #[tokio::main]
 async fn main() {
@@ -77,7 +56,9 @@ async fn main() {
     teloxide::enable_logging!();
     log::info!("Starting kitty...");
 
-    let bot = Bot::from_env();
+    let bot = BotBuilder::new()
+        .parse_mode(types::ParseMode::HTML)
+        .build();
 
     let check_kitties = kittenloop(bot.clone());
     let answer_message = teloxide::repl(bot, |message| async move {
